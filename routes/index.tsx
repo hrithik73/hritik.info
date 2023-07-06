a07a3fedfefcfb6e2d0a04d8dab664ae05e4d298
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-07-06 17:55:56 +0530

    feat: added 404 page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,6 @@
-import { Head } from "$fresh/runtime.ts";
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { Head } from '$fresh/runtime.ts';
+import { useSignal } from '@preact/signals';
+import Counter from '../islands/Counter.tsx';
 
 export default function Home() {
   const count = useSignal(3);
@@ -9,13 +9,13 @@ export default function Home() {
       <Head>
         <title>Fresh App</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
+      <div class='p-4 mx-auto max-w-screen-md '>
         <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
+          src='/logo.svg'
+          class='w-32 h-32'
+          alt='the fresh logo: a sliced lemon dripping with juice'
         />
-        <p class="my-6">
+        <p class='my-6'>
           Welcome to `fresh`. Try updating this message in the
           ./routes/index.tsx file, and refresh.
         </p>
---
+++ routes/projects.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <main>
+      <h1 class='text-xl'>Projects</h1>
+      <p>This is the project page.</p>
+    </main>
+  );
+}
